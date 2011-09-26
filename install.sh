34ff712075ccf29d9c5e7770a54c6af0572a1d76
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-09-26 13:10:53 +0100

    Removed old shell scripts

--- install.sh
+++
@@ -1,2 +0,0 @@
-mvn clean compile assembly:assembly
-#cp target/twitter-0.2-SNAPSHOT-jar*.jar ~/local/eclipse/com262lab1/lib/twitter-0.2.jar
--- sync.sh
+++
@@ -1,2 +0,0 @@
-mvn clean javadoc:javadoc
-rsync -avz --progress target/site/apidocs/ stulogin.dcs.shef.ac.uk:public_html/twitter/
