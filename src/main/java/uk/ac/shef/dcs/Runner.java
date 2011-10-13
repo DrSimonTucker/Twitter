a80628033781a765f3fa0606b29b3dafc7385ab9
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-10-13 22:19:49 +0100

    Fixed the creation of the lists

--- src/main/java/uk/ac/shef/dcs/Runner.java
+++ src/main/java/uk/ac/shef/dcs/Runner.java
@@ -26,32 +26,62 @@ public class Runner
       }
       if (args[0].equals("assign"))
       {
-         File f = new File("assign.data");
+         File f = new File("film.data");
+         File f2 = new File("music.data");
+         File f3 = new File("books.data");
          Runner mine = new Runner();
-         mine.collectAssignmentData(f);
+         mine.collectAssignmentData(f, f2, f3);
       }
       else if (args[0].equals("week1run"))
          Week1Framework.runWeek1(args[1]);
    }
 
-   public final void collectAssignmentData(final File outFile) throws IOException
-   {
-      PrintStream ps = new PrintStream(outFile);
-      SocialPost[] arr = TwitterProxy.getFeedList("guardian", "film-staff", 400);
-      for (SocialPost post : arr)
-         if (post != null)
-            ps.println("FB:::" + post.getUsername() + ":::" + post.getText());
-      arr = TwitterProxy.getFeedList("guardian", "music-staff", 400);
-      for (SocialPost post : arr)
-         if (post != null)
-            ps.println("TW:::" + post.getUsername() + ":::" + post.getSource() + ":::"
-                  + post.getText());
-      arr = TwitterProxy.getFeedList("guardian", "books-staff", 400);
-      for (SocialPost post : arr)
-         if (post != null)
-            ps.println("GP:::" + post.getUsername() + ":::" + post.getSource() + ":::"
-                  + post.getText());
+   int counter1 = 0;
+   int counter2 = 0;
 
+   public String buildResult(SocialPost post)
+   {
+      counter1++;
+      if (counter1 % 3 == 0)
+      {
+         counter2++;
+         if (counter2 % 3 == 0)
+            return "FB:::" + post.getUsername() + ":::WALL:::" + post.getText();
+         else if (counter2 % 3 == 1)
+            return "FB:::" + post.getUsername() + ":::DIRECT:::" + post.getText();
+         else
+            return "FB:::" + post.getUsername() + ":::GROUP:::" + post.getText();
+      }
+      else if (counter1 % 3 == 1)
+         return "TW:::" + post.getUsername() + ":::" + post.getText();
+      else
+         return "GP:::" + post.getUsername() + ":::" + post.getSource() + ":::" + post.getText();
+   }
+
+   public final void collectAssignmentData(final File filmOut, final File musicOut,
+         final File booksOut) throws IOException
+   {
+      int count = 100;
+
+      PrintStream ps = new PrintStream(filmOut);
+      SocialPost[] arr = TwitterProxy.getFeedList("guardian", "film-staff", count);
+      for (SocialPost post : arr)
+         if (post != null)
+            ps.println(buildResult(post));
+      ps.close();
+
+      ps = new PrintStream(musicOut);
+      arr = TwitterProxy.getFeedList("guardian", "music-staff", count);
+      for (SocialPost post : arr)
+         if (post != null)
+            ps.println(buildResult(post));
+      ps.close();
+
+      ps = new PrintStream(booksOut);
+      arr = TwitterProxy.getFeedList("guardian", "books-staff", count);
+      for (SocialPost post : arr)
+         if (post != null)
+            ps.println(buildResult(post));
       ps.close();
    }
 
